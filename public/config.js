bb4672a103eafdce55be0fc734efe84235043d71
Lars Lien <lars@familienlien.com>
2020-07-09 11:22:52 +0200

    Some more progress. Lots left to do

--- public/config.js
+++ public/config.js
@@ -3,4 +3,4 @@
     All env variables has to start with the "WORKOUT_" prefix to work in production.
     Production config are added at runtime, see nginx/start.sh
 */
-var WORKOUT_BACKEND_URI = "http://192.168.1.248:4000/api";
+var WORKOUT_BACKEND_URI = "http://localhost:4000/api";//"http://192.168.1.248:4000/api";
--- src/inputTable/components/sportSelect.jsx
+++ src/inputTable/components/sportSelect.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import sportsService from "../../common/services/sportsService";
 
 const SportSelect = ({ column }) => {
-  console.log(column);
+  const [sports, setSports] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      setSports(await sportsService.getSports());
+    }
+    fetch();
+  });
+
+  console.log(sports);
 
   return (
-    <select class="custom-select" id="inputGroupSelect01">
-      {
-      //!column.sport && <option selected value="">Select sport</option>
-      }
-
-      <option value="1">Løping</option>
-      <option value="2">Jogging</option>
-      <option value="3">Henge i et tre</option>
-    </select>
+    <div contentEditable={false}>
+      <select className="custom-select" id="inputGroupSelect01" value={column.sport._id}>
+        {sports.map((sport) => (
+          <option key={column._id + sport._id} value={sport._id}>{sport.name}</option>
+        ))}
+      </select>
+    </div>
   );
 };
 
